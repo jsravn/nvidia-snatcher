0c1804d2c875bd8cba2ab6198c7cb6e957123374
James Ravn <james@r-vn.org>
2020-10-10 16:47:08 +0100

    some shell improvements

--- shell.nix
+++ shell.nix
@@ -1,15 +1,7 @@
-let
-  pkgs = import <nixpkgs> { config.allowUnfree = true; };
-in
+{ pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   buildInputs = with pkgs; [
-		nodejs
+	  nodejs-12_x
   ];
-  shellHook = ''
-      export NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-      export NPM_CONFIG_CACHE      = "$XDG_CACHE_HOME/npm";
-      export NPM_CONFIG_TMP        = "$XDG_RUNTIME_DIR/npm";
-      export NPM_CONFIG_PREFIX     = "$XDG_CACHE_HOME/npm";
-      export NODE_REPL_HISTORY     = "$XDG_CACHE_HOME/node/repl_history";
-  '';
 }
